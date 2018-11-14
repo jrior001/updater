7b04c830379f10feb4178ff33974aafb0ff1b11c
Jason Riordan <jrior001@www.theflamingskull.com>
2018-11-14 17:20:25 -0500

    add script to fetch dependencies for easy setup

---
+++ fetch_deps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -o devices.json https://raw.githubusercontent.com/LineageOS/hudson/master/updater/devices.json
+curl -o device_deps.json https://raw.githubusercontent.com/LineageOS/hudson/master/updater/device_deps.json
